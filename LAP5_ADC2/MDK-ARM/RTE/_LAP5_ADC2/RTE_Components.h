8ccc7093d137935c000b130e3117e08455630ae4
Natthapol-PEET <natthapol.n@ku.th>
2020-02-08 23:28:09 +0700

    LAP05_ADC and LAB5_ADC2 push from PEET

---
+++ LAP5_ADC2/MDK-ARM/RTE/_LAP5_ADC2/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LAP5_ADC2' 
+ * Target:  'LAP5_ADC2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
